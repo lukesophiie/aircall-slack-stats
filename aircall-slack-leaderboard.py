927aea9cab15ccec3c2507a7d0d7e3f327fed0c9
lukesophiie <luke@sophiie.ai>
2026-02-03 13:15:35 +1000

    Update aircall-slack-leaderboard.py

--- aircall-slack-leaderboard.py
+++ aircall-slack-leaderboard.py
@@ -159,7 +159,8 @@ def main() -> None:
 
     lines = []
     lines.append(
-        f"<!sales_team> this is the current talk time stats so far today (Brisbane day) · {date_str} · up to {upto_str} (Brisbane)
+        f"<!sales_team> this is the current talk time stats so far today (Brisbane day) · {date_str} · up to {upto_str} (Brisbane) · {SCRIPT_VERSION}"
+    )
     lines.append("")
 
     if leaderboard:
